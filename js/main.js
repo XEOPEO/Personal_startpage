6fb514c957169d83ad4bcdb07537492cad0474e8
XeoXeo <ced2595@hotmail.com>
2016-06-27 00:35:07 +0200

    Added some comments

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
+// Author: Cedric 'Xeo' Dellisse
 $(document).ready(function() {
+    // Set clock element on actual time
     $('#clock').html(new Date().toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, "$1"));
+    // Update clock every second to stay up to date
     var interval = setInterval(function() {$('#clock').html(new Date().toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, "$1"));}, 1000);
 });
